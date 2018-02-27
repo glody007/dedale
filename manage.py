3cffa5334720a158dc25bd3914867056758fadaa
alchemist <glodymbutwile@gmail.com>
2018-02-27 16:07:55 -0500

    correction de l erreur d importation de School et AdminSchool

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import Student
+from app.models import Student, School, AdminSchool
 from flask_script import Manager, Shell
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app = app, db = db, Student = Student)
+    return dict(app = app, db = db, Student = Student, School = School, AdminSchool = AdminSchool)
 
 manager.add_command('shell', Shell(make_context = make_shell_context))
 
