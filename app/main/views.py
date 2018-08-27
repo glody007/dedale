36cd8972d5c60c690e4228b16ab410d74bc9c03d
dyglo <dyglo@localhost.localdomain>
2018-08-27 07:24:30 -0400

    add student view to the main route

--- app/main/views.py
+++ app/main/views.py
@@ -3,4 +3,12 @@ from . import main
 
 @main.route('/')
 def index():
-    return '<h1>Home page</h1>'
+    return render_template('home.html')
+
+@main.route('/student/<id>')
+def student(id):
+    from ..models import Student
+    student = Student.query.filter_by(id=int(id)).first()
+    if student is None:
+        abort(404)
+    return render_template('infostudent.html', name=student.first_name)
