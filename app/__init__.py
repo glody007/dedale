e590b92b14dc0a793dc2a0fd99372a8cc1116973
dyglo <dyglo@localhost.localdomain>
2018-10-11 15:15:21 -0400

    delete use auth in init file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy  import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from .main import main as main_blueprint
-from .auth import auth as auth_blueprint
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -18,7 +18,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
+
 
     db.init_app(app)
     login_manager.init_app(app)
