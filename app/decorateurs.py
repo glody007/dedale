9c6e0e20098c853f9fe68ae41535129022cd3575
dyglo <dyglo@localhost.localdomain>
2019-02-06 15:39:14 -0500

    ajout des decorateurs d administration

---
+++ app/decorateurs.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from flask import abort
+from flask_login import current_user
+
+def permission_requise(permission):
+    def decorateur(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.peut(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorateur
+
+def guru_requis(f):
+    return permission_requise(Permission.SUPPRIMER_ECOLE)(f)
+
+def moine_requis(f):
+    return permission_requise(Permission.MODIFIER_ECOLE)(f)
